dd144b7cb30f7ba48e186f7904a33b1149ea6d15
LotuxPunk <clement@vandendaelen.com>
2018-10-12 20:06:44 +0200

    change forge version

--- src/main/java/com/vandendaelen/handles/utils/Reference.java
+++ src/main/java/com/vandendaelen/handles/utils/Reference.java
@@ -4,9 +4,7 @@ public class Reference {
     public static final String MODID = "handles";
     public static final String MOD_NAME = "Handles";
     public static final String VERSION = "0.1";
-    public static final String DEP = "after:tardis, computercraft; required-after:forge@[14.23.4.2705,)";
+    public static final String DEP = "after:tardis, computercraft; required-after:forge@[14.23.4.2768,)";
     public static final String CLIENT_PROXY_CLASS = "com.vandendaelen.handles.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "com.vandendaelen.handles.proxy.ServerProxy";
-
-    public static String BASE_HANDLES_DIR = "./mods/handles/";
 }
