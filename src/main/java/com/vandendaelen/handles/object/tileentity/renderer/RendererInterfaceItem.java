b421fc844e751848943d077c972926e56164e929
LotuxPunk <clement@vandendaelen.com>
2018-10-12 11:31:12 +0200

    fix visual bug

--- src/main/java/com/vandendaelen/handles/object/tileentity/renderer/RendererInterfaceItem.java
+++ src/main/java/com/vandendaelen/handles/object/tileentity/renderer/RendererInterfaceItem.java
@@ -19,6 +19,8 @@ public class RendererInterfaceItem extends TileEntityItemStackRenderer {
     public void renderByItem(ItemStack itemStackIn) {
         GlStateManager.pushMatrix();
         mc.getTextureManager().bindTexture(RendererInterface.TEXTURE);
+        GlStateManager.translate(0.5,-0.75,0);
+        GlStateManager.rotate(0,0.0F, 0.0F, 0.0F);
         modelTardisInterface.render(null, 0, 0, 0, 0, 0, 0.0625F);
         GlStateManager.popMatrix();
     }
