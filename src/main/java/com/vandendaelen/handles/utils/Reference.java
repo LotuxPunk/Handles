378624719ba57585878f551220f58e0c42c1cb01
LotuxPunk <clement@vandendaelen.com>
2018-10-15 15:58:48 +0200

    version stuff

--- src/main/java/com/vandendaelen/handles/utils/Reference.java
+++ src/main/java/com/vandendaelen/handles/utils/Reference.java
@@ -12,7 +12,7 @@ public class Reference {
         public static final String MCVERSION = "1.12.2";
         public static final String MAJORMOD = "1";
         public static final String MAJORAPI = "1";
-        public static final String MINOR = "1";
+        public static final String MINOR = "2";
         public static final String PATCH = "0";
         public static final String VERSION = MCVERSION+"-"+MAJORMOD+"."+MAJORAPI+"."+MINOR+"."+PATCH;
     }
