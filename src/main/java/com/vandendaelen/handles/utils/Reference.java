49f61058ca2b3f0a48138ec0ddef1d69ec80bc84
LotuxPunk <clement@vandendaelen.com>
2018-11-01 14:18:12 +0100

    version bump

--- src/main/java/com/vandendaelen/handles/utils/Reference.java
+++ src/main/java/com/vandendaelen/handles/utils/Reference.java
@@ -17,8 +17,8 @@ public class Reference {
     public static class VERSION{
         public static final String MCVERSION = "1.12.2";
         public static final String MAJORMOD = "1";
-        public static final String MAJORAPI = "1";
-        public static final String MINOR = "2";
+        public static final String MAJORAPI = "2";
+        public static final String MINOR = "0";
         public static final String PATCH = "0";
         public static final String VERSION = MCVERSION+"-"+MAJORMOD+"."+MAJORAPI+"."+MINOR+"."+PATCH;
     }
