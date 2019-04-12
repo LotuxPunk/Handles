ba316156d7626a4b99f7498fafc75e9d121a50d5
LotuxPunk <vandendaelenclement@gmail.com>
2019-04-12 20:16:13 +0200

    update tardis mod dep

--- src/main/java/com/vandendaelen/handles/utils/Reference.java
+++ src/main/java/com/vandendaelen/handles/utils/Reference.java
@@ -18,7 +18,7 @@ public class Reference {
         public static final String MAJORMOD = "1";
         public static final String MAJORAPI = "5";
         public static final String MINOR = "1";
-        public static final String PATCH = "2";
+        public static final String PATCH = "3";
         public static final String VERSION = MCVERSION+"-"+MAJORMOD+"."+MAJORAPI+"."+MINOR+"."+PATCH;
     }
 }
