9b8a8866fea99390a2f77d3eb9b1f42fb4f792f8
50ap5ud5 <50ap5ud5125@gmail.com>
2021-06-06 12:47:14 +1000

    Make Aprioritron inherit TardisPartItem and add repair recipe

--- src/main/java/com/vandendaelen/handles/items/AprioritronItem.java
+++ src/main/java/com/vandendaelen/handles/items/AprioritronItem.java
@@ -1,14 +1,21 @@
 package com.vandendaelen.handles.items;
 
 import com.vandendaelen.handles.Handles;
+
 import net.minecraft.item.Item;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.tardis.mod.constants.Constants.Part.PartType;
+import net.tardis.mod.items.TardisPartItem;
 
-public class AprioritronItem extends Item {
+public class AprioritronItem extends TardisPartItem {
 
-    public AprioritronItem() {
-        super(new Item.Properties()
-                .maxStackSize(1)
-                .maxDamage(2500)
-                .group(Handles.setup.itemGroup));
-    }
+	public AprioritronItem(PartType type, boolean requiredForFlight, boolean requiresRepair,
+	        TranslationTextComponent dependentItem) {
+		super(new Item.Properties()
+              .maxStackSize(1)
+              .maxDamage(2500)
+              .group(Handles.setup.itemGroup), type, requiredForFlight, requiresRepair, dependentItem);
+	}
+
+    
 }
--- src/main/java/com/vandendaelen/handles/items/HandlesItems.java
+++ src/main/java/com/vandendaelen/handles/items/HandlesItems.java
@@ -6,10 +6,11 @@ import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.tardis.mod.constants.Constants;
 
 public class HandlesItems {
 	
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Handles.MODID);
 	
-	public static final RegistryObject<Item> APRIORITRON = ITEMS.register("aprioritron", () -> new AprioritronItem());
+	public static final RegistryObject<Item> APRIORITRON = ITEMS.register("aprioritron", () -> new AprioritronItem(Constants.Part.PartType.UPGRADE, false, true, Constants.Translations.DEMAT_CIRCUIT));
 }
