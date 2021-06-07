ed7f2847a332d9040afaaf082c21ecaa107aa56b
50ap5ud5 <50ap5ud5125@gmail.com>
2021-06-08 02:20:50 +1000

    Modify setFlight logic as requested

--- src/main/java/com/vandendaelen/handles/functions/handles/SetFlight.java
+++ src/main/java/com/vandendaelen/handles/functions/handles/SetFlight.java
@@ -7,7 +7,6 @@ import dan200.computercraft.api.lua.LuaException;
 import dan200.computercraft.api.lua.MethodResult;
 import net.minecraft.util.concurrent.TickDelayedTask;
 import net.tardis.mod.controls.ThrottleControl;
-import net.tardis.mod.subsystem.StabilizerSubsystem;
 import net.tardis.mod.tileentities.ConsoleTile;
 
 public class SetFlight implements IFunction {
@@ -29,15 +28,13 @@ public class SetFlight implements IFunction {
             speed = 1.0D;
         }
         ThrottleControl throttle = tardis.getControl(ThrottleControl.class).get();
-        StabilizerSubsystem stabilizer = tardis.getSubsystem(StabilizerSubsystem.class).orElse(null);
-        if (throttle != null & speed > 0) {
-            if (stabilizer.canBeUsed()) {
-                stabilizer.setControlActivated(true); //Always set stabilisers to true
-                throttle.setAmount((float)speed);
-                tardis.getLevel().getServer().addTickable(new TickDelayedTask(1, tardis::takeoff));
-            }
+        /* 50ap5ud5: As requested, make the Tardis takeoff regardless of stabilisers.
+         * This is to allow for users to manually set subsystem values in the future when we add the Subsystem State Modification feature.
+         */
+        if (throttle != null & speed > 0) { 
+            throttle.setAmount((float)speed);
+            tardis.getLevel().getServer().addTickable(new TickDelayedTask(1, tardis::takeoff)); //Schedule by 1 tick to allow the throttle animation to play correctly.
         }
-        
         return MethodResult.of();
     }
 }
