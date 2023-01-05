29ae8e11b5466a52725bb4d0bad9fdc6165cede2
Clément Vandendaelen <vandendaelenclement@gmail.com>
2023-01-05 22:59:03 +0100

    Fixed behavior of setSpeed. Close #51

--- src/main/java/com/vandendaelen/handles/functions/handles/SetSpeed.java
+++ src/main/java/com/vandendaelen/handles/functions/handles/SetSpeed.java
@@ -5,6 +5,8 @@ import com.vandendaelen.handles.helpers.FunctionHelper;
 import dan200.computercraft.api.lua.IArguments;
 import dan200.computercraft.api.lua.LuaException;
 import dan200.computercraft.api.lua.MethodResult;
+import net.minecraft.util.concurrent.TickDelayedTask;
+import net.tardis.mod.controls.HandbrakeControl;
 import net.tardis.mod.controls.ThrottleControl;
 import net.tardis.mod.tileentities.ConsoleTile;
 
@@ -15,9 +17,23 @@ public class SetSpeed implements IFunction {
     }
 
     @Override
-    public MethodResult run(ConsoleTile tardis, IArguments args) throws LuaException {
-        final ThrottleControl throttleControl = FunctionHelper.getTardisControl(tardis, ThrottleControl.class);
-        throttleControl.setAmount((float)(args.getDouble(0)));
+    public MethodResult run(ConsoleTile console, IArguments args) throws LuaException {
+        final var throttleControl = FunctionHelper.getTardisControl(console, ThrottleControl.class);
+        final var handbrakeControl = FunctionHelper.getTardisControl(console, HandbrakeControl.class);
+
+        final var speed = args.getDouble(0);
+        throttleControl.setAmount((float)(speed));
+
+        if (speed == 0 && console.isInFlight()) {
+            console.getLevel().getServer().tell(new TickDelayedTask(1, console::initLand));
+        } else {
+            if (handbrakeControl.isFree() && !console.isInFlight()) {
+                console.getLevel().getServer().tell(new TickDelayedTask(1, console::takeoff));
+            } else {
+                console.updateFlightTime();
+            }
+        }
+
         return MethodResult.of(throttleControl.getAmount());
     }
 }
